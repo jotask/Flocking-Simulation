5f8f9ea5795669f18ad192fb45422fb846bb67d0
Jose Vives Iznardo <jota_sk@hotmail.com>
2019-01-12 13:11:53 +0000

    adding types

---
+++ flocking_simulation/src/types.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <memory>
+
+namespace aiko
+{
+
+    template <class T>
+    using AikoUPtr = std::unique_ptr<T>;
+
+    template <class T>
+    using AikoPtr = std::shared_ptr<T>;
+
+}

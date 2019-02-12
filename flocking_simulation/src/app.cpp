85c82ea567ab364f2c54e67d0cdd80b21a188220
Jose Vives Iznardo <jota_sk@hotmail.com>
2019-02-12 15:29:47 +0000

    created basic app class

---
+++ flocking_simulation/src/app.cpp
@@ -0,0 +1,27 @@
+#include "app.hpp"
+
+#include <iostream>
+
+namespace aiko
+{
+    Application::Application(aiko::Config& cfg)
+        : m_engine(std::make_unique<Engine>(cfg))
+    {
+
+    }
+
+    void Application::run()
+    {
+
+        const auto exitStatus = m_engine->run();
+
+        std::cout << exitStatus << std::endl;
+
+    }
+
+    Engine* Application::getEngine()
+    {
+        return m_engine.get();
+    }
+
+}

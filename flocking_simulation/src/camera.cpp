6a00a4536ca1d8d378ab244b901b52a8dc169751
Jose Vives Iznardo <jota_sk@hotmail.com>
2019-02-16 14:31:23 +0000

    Update camera.cpp

--- flocking_simulation/src/camera.cpp
+++ flocking_simulation/src/camera.cpp
@@ -81,6 +81,7 @@ namespace aiko
     void Camera::update(const TimeStep & step)
     {
         orbitCameraMovement(step);
+        m_transform.applyTransform(m_cam);
     }
 
     float Camera::getFOV() const

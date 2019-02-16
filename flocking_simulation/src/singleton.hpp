18a466b8ab0a21a055953cbe2bf627959200096c
Jose Vives Iznardo <jota_sk@hotmail.com>
2019-02-16 13:47:24 +0000

    Update singleton.hpp

--- flocking_simulation/src/singleton.hpp
+++ flocking_simulation/src/singleton.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "types.hpp"
+#include <assert.h>
 
 namespace aiko
 {
@@ -24,6 +25,40 @@ namespace aiko
         virtual ~Singleton() = default;
 
     };
+
+    template <class T>
+    class SingletonCtr
+    {
+    public:
+
+        static T* it()
+        {
+            //assert(s_instance != nullptr);
+            return s_instance;
+        }
+
+        // SingletonCtr(const SingletonCtr&) = delete;
+        // SingletonCtr& operator= (const SingletonCtr) = delete;
+
+    protected:
+        SingletonCtr() = default;
+        virtual ~SingletonCtr()     = default;
+
+        // Lazzy class just for this
+        // Previous pointer create a copy if called in constructor
+        static void setPtr(T* ptr)
+        {
+            s_instance = ptr;
+        };
+
+    private:
+
+        static T * s_instance;
+
+    };
+
+    template < class T >
+    T * SingletonCtr<T>::s_instance = nullptr;
 
 }
 

413747eeb0de1c7ab5e1044a4cd31d8e36124a35
Jose Vives Iznardo <jota_sk@hotmail.com>
2019-02-16 14:27:29 +0000

    Update entity_system.hpp

--- flocking_simulation/src/entity/entity_system.hpp
+++ flocking_simulation/src/entity/entity_system.hpp
@@ -34,12 +34,12 @@ namespace aiko
 
     public:
 
-        template<class T>
-        static T* createEntity()
+        template<class T, class... Args >
+        static T* createEntity(Args &&...args)
         {
             static_assert(std::is_base_of<Entity, T>::value, "T is not base class off Entity");
             auto* entitySystem = EntitySystem::it();
-            auto it = std::make_unique<T>();
+            auto it = std::make_unique<T>(std::forward<Args>(args)...);
             entitySystem->m_entities.push_back(std::move(it));
             auto& tmp = EntitySystem::it()->m_entities.back();
             auto* ptr = tmp.get();

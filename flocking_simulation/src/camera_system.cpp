74db8150baa0328443d4189a19decdaa9e686349
Jose Vives Iznardo <jota_sk@hotmail.com>
2019-02-16 14:27:24 +0000

    update camera system

--- flocking_simulation/src/camera_system.cpp
+++ flocking_simulation/src/camera_system.cpp
@@ -1,5 +1,6 @@
 #include "camera_system.hpp"
 
+#include "camera.hpp"
 #include "engine.hpp"
 #include "renderer.hpp"
 #include "input.hpp"
@@ -9,18 +10,15 @@
 #include "event/event_dispatcher.hpp"
 #include "utils.hpp"
 
+#include "entity/entity_system.hpp"
+
 namespace aiko
 {
 
-    CameraSystem::CameraSystem(float fov, float nearPlane, float farPlane)
-        :   System()
-        , m_transform()
-        , m_cam(0)
+    CameraSystem::CameraSystem()
+        : System()
         , m_renderer(nullptr)
-        , m_fov(fov)
-        , m_nearPlane(nearPlane)
-        , m_farPlane(farPlane)
-        , m_velocity(0.001f)
+        , m_cameras()
     {
 
     }
@@ -34,44 +32,17 @@ namespace aiko
     void CameraSystem::init()
     {
 
-        if (true)
-        {
-            // setup: create an anchor node, so that the camera can always rotate around a relative(0, 0, 0) point
-            m_anchor = h3dAddGroupNode(H3DRootNode, "camera-anchor");
-            m_cam = h3dAddCameraNode(m_anchor, "camera-orbiter", m_renderer->getPipelineRes());
-
-            m_transform.m_position.x = 5;
-            m_transform.m_position.y = 3;
-            m_transform.m_position.z = 19;
-
-        }
-
-        if (false)
-        {
-
-            // TODO add error checking
-            // Add camera
-            m_cam = h3dAddCameraNode(H3DRootNode, "Camera", m_renderer->getPipelineRes());
-
-
-            m_transform.m_position.x =  5;
-            m_transform.m_position.y =  3;
-            m_transform.m_position.z = 19;
-
-            m_transform.m_rotation.x =  7;
-            m_transform.m_rotation.y = 15;
-        }
-
-        h3dSetNodeParamI(m_cam, H3DCamera::OccCullingI, 1);
-
         EventSystem::it().bind<WindowResizeEvent>(this, &CameraSystem::onWindowResize);
         EventSystem::it().bind<OnMouseMoveEvent>(this, &CameraSystem::onMouseMove);
+
+        auto* camera = EntitySystem::createEntity<Camera>(*m_renderer, *m_input);
+        m_cameras.push_back(camera);
 
     }
 
     void CameraSystem::update(const TimeStep & step)
     {
-        orbitCameraMovement(step);
+
     }
 
     void CameraSystem::postUpdate(const TimeStep & step)
@@ -81,42 +52,13 @@ namespace aiko
         // post update call applyTransform that passed an hd3 node to apply a transform?
 
         // Set camera parameters
-        m_transform.applyTransform(m_cam);
+        // m_transform.applyTransform(m_cam);
     }
 
-    float CameraSystem::getFOV() const
+    const Camera& CameraSystem::getCamera() const
     {
-        return m_fov;
-    }
-
-    float CameraSystem::getNearPlane() const
-    {
-        return m_nearPlane;
-    }
-
-    float CameraSystem::getFarPlane() const
-    {
-        return m_farPlane;
-    }
-
-    const H3DNode& CameraSystem::getCamera() const
-    {
-        return m_cam;
-    }
-
-    void CameraSystem::lookAt(const glm::vec3 target)
-    {
-
-        auto& position = m_transform.m_position;
-        auto& rotation = m_transform.m_rotation;
-
-        const auto vXPos = target.x - position.x;
-        const auto vYPos = target.y - position.y;
-        const auto vZPos = target.z - position.z;
-
-        rotation.x = utils::radToDeg(atan2(vYPos, sqrtf(vXPos * vXPos + vZPos * vZPos)));
-        rotation.y = utils::radToDeg(atan2(-vXPos, -vZPos));
-
+        assert(m_cameras.empty() == false);
+        return *m_cameras.back();
     }
 
     void CameraSystem::onMouseMove(Event& event)
@@ -143,74 +85,10 @@ namespace aiko
 
     void CameraSystem::resizeViewport(const int width, const int height)
     {
-        // Resize viewport
-        h3dSetNodeParamI(m_cam, H3DCamera::ViewportXI, 0);
-        h3dSetNodeParamI(m_cam, H3DCamera::ViewportYI, 0);
-        h3dSetNodeParamI(m_cam, H3DCamera::ViewportWidthI, width);
-        h3dSetNodeParamI(m_cam, H3DCamera::ViewportHeightI, height);
-    
-        // Set virtual camera parameters
-        h3dSetupCameraView(m_cam, m_fov, (float)width / height, m_nearPlane, m_farPlane);
-    }
-
-    void CameraSystem::controlCameraMovement(const TimeStep & step)
-    {
-        #define H3D_RAD2DEG 57.324840764f
-        #define H3D_DEG2RAD  0.017453292f
-
-        float curVel = m_velocity / step.getDelta();
-
-        auto& p = m_transform.m_position;
-        auto& r = m_transform.m_rotation;
-
-        if (m_input->isKeyDown(GLFW_KEY_LEFT_SHIFT)) curVel *= 5;	// LShift
-
-        if (m_input->isKeyDown(GLFW_KEY_W))
+        for (auto* cam : m_cameras)
         {
-            // Move forward
-            p.x -= sinf(H3D_DEG2RAD * (r.y)) * cosf(-H3D_DEG2RAD * (r.x)) * curVel;
-            p.y -= sinf(-H3D_DEG2RAD * (r.x)) * curVel;
-            p.z -= cosf(H3D_DEG2RAD * (r.y)) * cosf(-H3D_DEG2RAD * (r.x)) * curVel;
+            cam->resizeViewport(width, height);
         }
-
-        if (m_input->isKeyDown(GLFW_KEY_S))
-        {
-            // Move backward
-            p.x += sinf(H3D_DEG2RAD * (r.y)) * cosf(-H3D_DEG2RAD * (r.x)) * curVel;
-            p.y += sinf(-H3D_DEG2RAD * (r.x)) * curVel;
-            p.z += cosf(H3D_DEG2RAD * (r.y)) * cosf(-H3D_DEG2RAD * (r.x)) * curVel;
-        }
-
-        if (m_input->isKeyDown(GLFW_KEY_A))
-        {
-            // Strafe left
-            p.x += sinf(H3D_DEG2RAD * (r.y - 90)) * curVel;
-            p.z += cosf(H3D_DEG2RAD * (r.y - 90)) * curVel;
-        }
-
-        if (m_input->isKeyDown(GLFW_KEY_D))
-        {
-            // Strafe right
-            p.x += sinf(H3D_DEG2RAD * (r.y + 90)) * curVel;
-            p.z += cosf(H3D_DEG2RAD * (r.y + 90)) * curVel;
-        }
-    }
-
-    void CameraSystem::orbitCameraMovement(const TimeStep & step)
-    {
-
-        static auto dt = 0.0f;
-        dt += step.getDelta();
-
-        static const auto radius = 5.0f;
-
-        auto& p = m_transform.m_position;
-
-        p.x = radius * cos(dt);
-        p.z = radius * sin(dt);
-
-        lookAt();
-
     }
 
 }
--- flocking_simulation/src/camera_system.hpp
+++ flocking_simulation/src/camera_system.hpp
@@ -2,10 +2,9 @@
 
 #include "transform.hpp"
 #include "system.hpp"
+#include "camera.hpp"
 
-#include "Horde3D.h"
-
-#include "glm.hpp"
+#include <vector>
 
 namespace aiko
 {
@@ -20,7 +19,7 @@ namespace aiko
     {
     public:
 
-        CameraSystem(float fov = 45.0f, float near_plane = 0.1f, float far_plane = 1000.0f);
+        CameraSystem();
 
         virtual ~CameraSystem() = default;
 
@@ -30,16 +29,7 @@ namespace aiko
         virtual void        update(const TimeStep& step) override;
         virtual void        postUpdate(const TimeStep& step) override;
 
-        float               getFOV() const;
-        float               getNearPlane() const;
-        float               getFarPlane() const;
-
-        const H3DNode&      getCamera() const;
-
-
-        Transform           m_transform;
-
-        void                lookAt(const glm::vec3 target = glm::vec3());
+        const Camera&      getCamera() const;
 
     private:
 
@@ -47,22 +37,12 @@ namespace aiko
         void                onWindowResize(Event&);
         void                resizeViewport(const int width, const int height);
 
-        // Movement
-        void                controlCameraMovement(const TimeStep & step);
-        void                orbitCameraMovement(const TimeStep & step);
-
-        H3DNode             m_cam;
-        H3DNode             m_anchor;
-
         // Systems
         Renderer*           m_renderer;
         Input*              m_input;
 
-        float               m_fov;
-        float               m_nearPlane;
-        float               m_farPlane;
-
-        float               m_velocity; // Velocity for movement. Temporally until better system
+        using Cameras       = std::vector<Camera*>;
+        Cameras             m_cameras;
 
     };
 

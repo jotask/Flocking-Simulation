0f7e09efb7e73afe58c467f5257e4c152bdd99b4
Jose Vives Iznardo <jota_sk@hotmail.com>
2019-02-18 20:43:33 +0000

    Update mesh.cpp

--- flocking_simulation/src/entity/mesh.cpp
+++ flocking_simulation/src/entity/mesh.cpp
@@ -27,37 +27,11 @@ namespace aiko
     void Mesh::init()
     {
 
-        static auto posData = std::array<float, 12>({
-                                         0,  0, 0,
-                                        10,  0, 0,
-                                         0, 10, 0,
-                                        10, 10, 0
-                                        });
+    }
 
-        static auto indexData = std::array<unsigned int, 12>({ 0, 1, 2, 2, 1, 3 });
-
-        static auto normalData = std::array<short, 12>({
-            0, 0, 1,
-            0, 0, 1,
-            0, 0, 1,
-            0, 0, 1
-            });
-
-        static auto uvData = std::array<float, 8>({
-            0, 0,
-            1, 0,
-            0, 1,
-            1, 1
-            });
-
-        auto* assetsSystem = AssetsSystem::it();
-        const auto lightMatRes = assetsSystem->loadResource("materials/light.material.xml", H3DResTypes::Material);
-        const auto matRes = assetsSystem->loadResource("materials/testpattern.material.xml", H3DResTypes::Material);
-
-        H3DRes geoRes = h3dutCreateGeometryRes("geoRes", 4, 6, posData.data(), indexData.data(), normalData.data(), 0, 0, uvData.data(), 0);
-        H3DNode model = h3dAddModelNode(H3DRootNode, "DynGeoModelNode", geoRes);
-        m_mesh = h3dAddMeshNode(model, "DynGeoMesh", matRes, 0, 6, 0, 3);
-
+    H3DNode Mesh::getId()
+    {
+        return m_mesh;
     }
 
 }
